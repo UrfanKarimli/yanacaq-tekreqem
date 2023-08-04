3bc07ec351f39e7bf0d2f521de03c2d5c22ab8b2
UrfanKarimli <urfankarim@gmail.com>
2023-08-04 23:01:49 +0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function carTest(fuel, distance , price ) {
+    function carTest( ) {
+    let fuel = 8 ;
+    let distance = 100;
+    // let price = 2;
+    // cost = (fuel * price)
+    let result = (fuel / (distance/100))
+    console.log(result)
+
+
+    // carTest (cost)
+}
+
+
+
+{
+    let number = prompt("ədədi daxil edin")
+
+    number % 2 === 0 ?
+        console.log("cut ededdir") :
+        console.log("tek ededdir")
+}   
